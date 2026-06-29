46c64e5372a723096d01b2ac69c8a88778f73306
Kunsh Sabharwal <sabharwalkunsh05@gmail.com>
2026-06-29 11:35:25 +0530

    Day 4

--- Day 4 (29th June)/FactorialUptoN.cpp
+++ Day 4 (29th June)/FactorialUptoN.cpp
@@ -3,20 +3,12 @@ using namespace std;
 
 int factorialuptoN(int n)
 {
-    if (n <= 0)
+    int fact = 1;
+    for (int i = n; i > 0; i--)
     {
-        cout << "Enter a valid postive integer.";
-        return 0;
+        fact = fact * i;
     }
-    else
-    {
-        int fact = 1;
-        for (int i = n; i > 0; i--)
-        {
-            fact = fact * i;
-        }
-        return fact;
-    }
+    return fact;
     return 0;
 }
 int main()
@@ -24,6 +16,11 @@ int main()
     int n;
     cout << "Enter the number N to print factorial upto N: ";
     cin >> n;
+    if (n <= 0)
+    {
+        cout << "Enter a valid postive integer.";
+        return 0;
+    }
     for (int i = 1; i <= n; i++)
     {
         int ans = factorialuptoN(i);
