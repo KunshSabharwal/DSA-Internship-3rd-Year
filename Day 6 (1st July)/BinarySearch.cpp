807b0f70fe378d1643ee1c8b47bd85a486be30cf
Kunsh Sabharwal <sabharwalkunsh05@gmail.com>
2026-07-01 12:14:51 +0530

    Day 6

--- Day 6 (1st July)/BinarySearch.cpp
+++ Day 6 (1st July)/BinarySearch.cpp
@@ -17,6 +17,49 @@ int binary_search(vector<int> arr, int target)
     }
     return -1;
 }
+
+int binary_search_first_occurence(vector<int> arr, int target)
+{
+    int left = 0;
+    int right = arr.size() - 1;
+    int idx = -1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target)
+        {
+            idx = mid;
+            right = mid - 1;
+        }
+        else if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return idx;
+}
+
+int binary_search_last_occurence(vector<int> arr, int target)
+{
+    int left = 0;
+    int right = arr.size() - 1;
+    int idx = -1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target)
+        {
+            idx = mid;
+            left = mid + 1;
+        }
+        else if (arr[mid] > target)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+    return idx;
+}
+
 int main()
 {
     int n;
@@ -31,7 +74,7 @@ int main()
     int v;
     cout << "Enter the target value to search for: ";
     cin >> v;
-    int ans = binary_search(vec1, v);
+    int ans = binary_search_last_occurence(vec1, v);
     if (ans == -1)
         cout << "Entered target element not found in the array";
     else
