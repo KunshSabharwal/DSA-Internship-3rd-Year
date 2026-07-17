2edbfcc4c6628eff98b601572e61535083b16ac4
Kunsh Sabharwal <sabharwalkunsh05@gmail.com>
2026-07-17 13:27:26 +0530

    Day 18

--- Day 18 (17th July)/ClassesObjects.cpp
+++ Day 18 (17th July)/ClassesObjects.cpp
@@ -36,34 +36,25 @@ int main()
     // Object created on Stack
     // -----------------------------
     A obj1;
-
     cout << "Default Constructor (Stack Object)\n";
     obj1.print();
-
     cout << "\n";
-
     // -----------------------------
     // Parameterized Constructor
     // Object on Stack
     // -----------------------------
     A obj2(10, "Hello", 99.5);
-
     cout << "Parameterized Constructor (Stack Object)\n";
     obj2.print();
-
     cout << "\n";
-
     // -----------------------------
     // Object created on Heap
     // -----------------------------
     A *obj3 = new A(20, "World", 85.75);
-
     cout << "Parameterized Constructor (Heap Object)\n";
     obj3->print();            // Access function using ->
     cout << obj3->st << endl; // Access member variable using ->
-
     // Free heap memory
     delete obj3;
-
     return 0;
 }
